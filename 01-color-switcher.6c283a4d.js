7071e2a9dd6d2a6f2f6ad92254e1fa400a9b0093
Ogrodowczyk87 <63356079+Ogrodowczyk87@users.noreply.github.com>
2023-03-20 16:02:10 +0000

    Deploying to gh-pages from @ Ogrodowczyk87/goit-js-hw-09@45b68d234c153c3a392b1bb834dee31ea85fc8de 🚀

---
+++ 01-color-switcher.6c283a4d.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),n=document.querySelector("body"),o=null;t.addEventListener("click",(function(){o=setInterval((function(){n.style.backgroundColor="".concat("#".concat(Math.floor(823321141*Math.random()).toString(16)))}),1e3)})),e.addEventListener("click",(function(){clearInterval(o)}))}();
+//# sourceMappingURL=01-color-switcher.6c283a4d.js.map

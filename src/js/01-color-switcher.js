45b68d234c153c3a392b1bb834dee31ea85fc8de
Rafal Ogrodowczyk <r.ogrodowczyk87@gmail.com>
2023-03-20 16:01:44 +0000

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+const btnStart = document.querySelector('[data-start]');
 
+const btnStop = document.querySelector('[data-stop]');
+
+const body = document.querySelector('body');
+
+let timer = null;
+
+function getRandomHexColor() {
+    const randomColor = `#${Math.floor(Math.random() * 823321141).toString(16)}`;
+    return randomColor;
+}
+
+btnStart.addEventListener('click', () => {
+    timer = setInterval(() => {
+        body.style.backgroundColor = `${getRandomHexColor()}`;
+    }, 1000);
+});
+
+btnStop.addEventListener('click', () => {
+    clearInterval(timer);
+});
